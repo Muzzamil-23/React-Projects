d50dccedde260c963ef1b0a3e86e93d881ed3015
Muzzammil Siddiqui <muzzamil.siddiqui20@gmail.com>
2025-06-20 00:49:38 +0500

    Add environment configuration and AppWrite integration

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log(import.meta.env.VITE_APPWRITER_URL)
+
   return (
     <>
       <h1 className='bg-black text-white min-h-screen flex items-center justify-center text-6xl'>Test</h1>
