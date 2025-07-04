691adfc65527a4146299288446ff11f358466841
Muzzammil Siddiqui <muzzamil.siddiqui20@gmail.com>
2025-07-05 03:01:24 +0500

    Refactor Home component to improve post loading logic, enhance loading state handling, and streamline no posts available message

--- BlogApp/src/pages/Home.jsx
+++ BlogApp/src/pages/Home.jsx
@@ -1,42 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import service from '../appwrite/config'
 import { Button, Container, PostCard } from '../components'
-import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 
 const Home = () => {
     const [posts, setPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
     const navigate = useNavigate()
     useEffect(() => {
         service.getPosts().then((posts) => {
-            if(posts && posts.length > 0) {
-                setPosts(posts)
+            if (posts && posts.documents) {
+                setPosts(posts.documents)
             }
+            setIsLoading(false)
+        }).catch((error) => {
+            console.log("Appwrite Service :: getPosts :: error", error);
         })
     }, [])
 
-    if(posts.length <= 0) {
+    if (!isLoading && posts.length === 0) {
         return (
             <div className='w-full py-8 mt-4 text-center'>
                 <Container>
-                    <div className='flex flex-wrap'>
-                        <div className='p-2 w-full'>
-                            <h1 className='text-2xl font-bold hover:text-gray-500'>
-                                No Posts Available, be the first to write a blog!
-                            </h1>
-                            <button onClick={() => navigate("/add-post")}>Add post</button>
-                        </div>
-                    </div>
+                    <h1 className='text-2xl font-bold'>
+                        No Posts Available, be the first to write a blog!
+                    </h1>
+                     <Button className='mt-5 cursor-pointer' onClick={() => navigate("/add-post")}>Add Post</Button>
                 </Container>
             </div>
         )
     }
 
-    return (
+    return isLoading ? (<div className='w-full py-8 mt-4 text-center'>
+        <Container>
+            <div className='flex flex-wrap'>
+                <div className='p-2 w-full'>
+                    <h1 className='text-2xl font-bold hover:text-gray-500'>
+                        Loading......
+                    </h1>
+                </div>
+            </div>
+        </Container>
+    </div>) : (
         <div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
-                    {posts.map((post) => (
+                    {posts?.map((post) => (
                         <div key={post.$id} className='p-2 w-1/4'>
                             <PostCard {...post} />
                         </div>
